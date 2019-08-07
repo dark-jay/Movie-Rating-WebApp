536787c63f8a36f31dcbe98c6ec585e47b6e793e
jay nath <jay14nath@gmail.com>
2019-08-07 23:30:47 +0530

    different components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import HeroImage from '../elements/HeroImage/HeroImage';
+import SearchBar from '../elements/SearchBar/SearchBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import MovieThumb from '../elements/MovieThumb/MoiveThumb';
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
+import Spinner from '../elements/Spinner/Spinner';
 import './Home.css';
 
 class Home extends Component {
